3cc994332c74f82d4d780ab30b9ca27060e6bb85
Jordan Wolter <jordan.wolter@icloud.com>
2022-10-24 14:32:53 -0500

    made background

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/_120660639_thumbnail_chloesideeye_clean.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/5469e8fd-6de1-482f-88db-ec69d0c139f8_funny-weekend-memes-48-720x847.avif', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/8429942830ee706352fa51d1af23d503.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 4, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 5, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
 ];
--- src/components/GalleryImage/GalleryImage.jsx
+++ src/components/GalleryImage/GalleryImage.jsx
@@ -2,23 +2,19 @@ import { useState } from "react";
 
 
 function GalleryImage({ image, putLikes }) {
-    // console.log('IMAGE', image);
 
     const [commentBool, setCommentBool] = useState(false);
 
-    // console.log('COMMENT BOOL', commentBool);
-
     return (
         <>
-        <td className="box">
-        <div className="frame" onClick={() => setCommentBool( !commentBool )}>
-                {commentBool ? <p className="discription">{image.description}</p> : <img className="pic" src={image.path} />}
-            </div>
+            <td className="box">
+                <div className="frame" onClick={() => setCommentBool(!commentBool)}>
+                    {commentBool ? <p className="discription">{image.description}</p> : <img className="pic" src={image.path} />}
+                </div>
 
-            <h2 className="like">Likes: {image.likes}</h2>
-            <button className="button" onClick={() => putLikes(image.id)}>Like</button>
-        </td>
-            
+                <h2 className="like">Likes: {image.likes}</h2>
+                <button className="button" onClick={() => putLikes(image.id)}>Like</button>
+            </td>
         </>
     )
 }
